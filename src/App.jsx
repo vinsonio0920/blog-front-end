c794308eed75d3ed518cc744901d7a9ffa65ebc6
Vinson <vinsonhe2@gmail.com>
2026-03-27 08:49:46 -0700

    feat(homepage): add homepage content

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,109 @@
+import { Link } from "react-router";
+import {
+  blenderSvg,
+  githubSvg,
+  linkedinSvg,
+  logoSvg,
+  robloxSvg,
+  stackoverflowSvg,
+} from "./assets";
 import "./App.css";
 
 function App() {
   return (
     <>
-      <header>Header</header>
+      <header>
+        <nav>
+          <li>
+            <Link to="/">
+              <img src={logoSvg} width="50" alt="Logo SVG" />
+              Vinson Blogs
+            </Link>
+          </li>
+          <li>
+            <ul className="right-header">
+              <li>
+                <Link to="/Journal">Journal</Link>
+              </li>
+              <li>
+                <Link to="/articles">Articles</Link>
+              </li>
+              <li>
+                <Link to="About">About</Link>
+              </li>
+              <li>
+                <Link to="/sign-in">Sign In</Link>
+              </li>
+            </ul>
+          </li>
+        </nav>
+      </header>
       <p>This is where the router comes in handy!</p>
-      <footer>Footer</footer>
+      <footer>
+        <nav>
+          <li>
+            <ul className="social-links">
+              <li>
+                <Link to="">
+                  <img
+                    src={githubSvg}
+                    width="50"
+                    alt="My GitHub account link"
+                  />
+                </Link>
+              </li>
+              <li>
+                <Link to="">
+                  <img
+                    src={linkedinSvg}
+                    width="50"
+                    alt="My LinkedIn account link"
+                  />
+                </Link>
+              </li>
+              <li>
+                <Link to="">
+                  <img
+                    src={stackoverflowSvg}
+                    width="50"
+                    alt="My StackOverflow account link"
+                  />
+                </Link>
+              </li>
+              <li>
+                <Link to="">
+                  <img
+                    src={robloxSvg}
+                    width="50"
+                    alt="Link to my roblox games"
+                  />
+                </Link>
+              </li>
+              <li>
+                <Link to="">
+                  <img
+                    src={blenderSvg}
+                    width="50"
+                    alt="Link to my blender creations"
+                  />
+                </Link>
+              </li>
+            </ul>
+          </li>
+          <li>
+            <ul className="other-links">
+              <li>
+                <Link to="/attributions">Attributions</Link>
+                <Link to="/about">About</Link>
+                <Link to="/contact">Contact</Link>
+                <Link to="/roadmap">Roadmap</Link>
+                <Link to="/current">Current Projects</Link>
+              </li>
+            </ul>
+          </li>
+        </nav>
+        <p>Copyright ©2026; Vinson Blogs</p>
+      </footer>
     </>
   );
 }
---
+++ src/assets/index.js
@@ -0,0 +1,16 @@
+// allows images to be imported in one line instead of multiple
+import logoSvg from "./logo.svg";
+import blenderSvg from "./social-media/blender.svg";
+import githubSvg from "./social-media/github.svg";
+import linkedinSvg from "./social-media/linkedin.svg";
+import robloxSvg from "./social-media/roblox.svg";
+import stackoverflowSvg from "./social-media/stackoverflow.svg";
+
+export {
+  logoSvg,
+  blenderSvg,
+  githubSvg,
+  linkedinSvg,
+  robloxSvg,
+  stackoverflowSvg,
+};
