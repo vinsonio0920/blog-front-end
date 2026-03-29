9719065be2c2e823e51546eeaa40e2bef547aab1
Vinson <vinsonhe2@gmail.com>
2026-03-28 23:23:39 -0700

    style(footer): add mobile styling

--- src/footer/Footer.jsx
+++ src/footer/Footer.jsx
@@ -1,4 +1,5 @@
 import { Link } from "react-router";
+import "./Footer.css";
 import {
   blenderSvg,
   githubSvg,
@@ -11,60 +12,71 @@ const Footer = () => {
   return (
     <footer>
       <nav>
-        <li>
-          <ul className="social-links">
-            <li>
-              <Link to="">
-                <img src={githubSvg} width="50" alt="My GitHub account link" />
-              </Link>
-            </li>
-            <li>
-              <Link to="">
-                <img
-                  src={linkedinSvg}
-                  width="50"
-                  alt="My LinkedIn account link"
-                />
-              </Link>
-            </li>
-            <li>
-              <Link to="">
-                <img
-                  src={stackoverflowSvg}
-                  width="50"
-                  alt="My StackOverflow account link"
-                />
-              </Link>
-            </li>
-            <li>
-              <Link to="">
-                <img src={robloxSvg} width="50" alt="Link to my roblox games" />
-              </Link>
-            </li>
-            <li>
-              <Link to="">
-                <img
-                  src={blenderSvg}
-                  width="50"
-                  alt="Link to my blender creations"
-                />
-              </Link>
-            </li>
-          </ul>
-        </li>
-        <li>
-          <ul className="other-links">
-            <li>
-              <Link to="/attributions">Attributions</Link>
-              <Link to="/about">About</Link>
-              <Link to="/contact">Contact</Link>
-              <Link to="/roadmap">Roadmap</Link>
-              <Link to="/current">Current Projects</Link>
-            </li>
-          </ul>
-        </li>
+        <ul>
+          <li>
+            <ul className="social-links">
+              <li>
+                <Link to="">
+                  <img
+                    src={githubSvg}
+                    width="50"
+                    alt="My GitHub account link"
+                  />
+                </Link>
+              </li>
+              <li>
+                <Link to="">
+                  <img
+                    src={linkedinSvg}
+                    width="50"
+                    alt="My LinkedIn account link"
+                  />
+                </Link>
+              </li>
+              <li>
+                <Link to="">
+                  <img
+                    src={stackoverflowSvg}
+                    width="50"
+                    alt="My StackOverflow account link"
+                  />
+                </Link>
+              </li>
+              <li>
+                <Link to="">
+                  <img
+                    src={robloxSvg}
+                    width="50"
+                    alt="Link to my roblox games"
+                    className="roblox-svg"
+                  />
+                </Link>
+              </li>
+              <li>
+                <Link to="">
+                  <img
+                    src={blenderSvg}
+                    width="50"
+                    alt="Link to my blender creations"
+                  />
+                </Link>
+              </li>
+            </ul>
+          </li>
+          <li>
+            <ul className="other-links">
+              <li>
+                <Link to="/attributions">Attributions</Link>
+                <Link to="/about">About</Link>
+                <Link to="/contact">Contact</Link>
+                <Link to="/roadmap">Roadmap</Link>
+                <Link to="/current">Current Projects</Link>
+              </li>
+            </ul>
+          </li>
+        </ul>
       </nav>
-      <p>Copyright ©2026; Vinson Blogs</p>
+      <p className="copyright-statement">Copyright ©2026; Vinson Blogs</p>
     </footer>
   );
 };
