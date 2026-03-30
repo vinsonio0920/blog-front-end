8432f537290d8e1e90a732a2ab1fb07d9d7d4ad5
Vinson <vinsonhe2@gmail.com>
2026-03-30 10:06:53 -0700

    style(error): add responsive styling

--- src/error/ErrorPage.jsx
+++ src/error/ErrorPage.jsx
@@ -1,11 +1,21 @@
 import { Link } from "react-router";
+import styles from "./ErrorPage.module.css";
+import { logoSvg } from "../assets";
 
 const ErrorPage = () => {
   return (
-    <div>
-      <h1>Error 404</h1>
-      <p>The page you are looking for doesn't exist.</p>
-      <Link to="/">Go back to the homepage</Link>
+    <div className={styles.pageContainer}>
+      <div className={styles.errorContainer}>
+        <h1 className={styles.errorHeading}>Error 404</h1>
+        <p>The page you are looking for doesn't exist.</p>
+        <Link to="/" className={styles.homepageLink}>
+          Go back to the homepage
+        </Link>
+        <div className={styles.logoContainer}>
+          <img src={logoSvg} width="30" alt="Logo SVG" className="logo-svg" />
+          <span className="logo-text">Vinson Blogs</span>
+        </div>
+      </div>
     </div>
   );
 };
