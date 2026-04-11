849f1ad621d036cee16aaad3a3b9d74e6c521f63
Vinson <vinsonhe2@gmail.com>
2026-04-11 12:56:04 -0700

    feat(auth): sign out expired jwtTokens
    When the JWT token is expired, automatically remove the token from local
    storage to prevent any unexpected bugs in the future

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,24 @@ import "./App.css";
 import { Header } from "./header/Header";
 import { Footer } from "./footer/Footer";
 import { JwtContext } from "./jwt-context";
+import { jwtDecode } from "jwt-decode";
 import { useState } from "react";
 
 function App() {
   const [jwtToken, setJwtToken] = useState(localStorage.getItem("jwtToken"));
   const value = { jwtToken, setJwtToken };
+
+  // check if token is expired
+  if (jwtToken) {
+    const decoded = jwtDecode(jwtToken);
+    const currentDate = new Date();
+    const secondsSinceEpoch = currentDate.getTime() / 1000;
+    if (secondsSinceEpoch > decoded.exp) {
+      console.log("Expired!");
+      localStorage.removeItem("jwtToken");
+      setJwtToken(null);
+    }
+  }
 
   return (
     <JwtContext value={value}>
