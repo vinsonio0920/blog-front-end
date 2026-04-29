8584669a20f5dcb9ce306c04cc54b2049c27e932
Vinson <vinsonhe2@gmail.com>
2026-04-29 15:44:03 -0700

    feat(attributions): add attributions JSX

--- src/attributions/Attributions.jsx
+++ src/attributions/Attributions.jsx
@@ -1,5 +1,101 @@
 const Attributions = () => {
-  return <p>Attributions</p>;
+  return (
+    <>
+      <h1>Attributions</h1>
+      <ul>
+        <li>
+          Empty articles construction image by{" "}
+          <a
+            target="_blank"
+            href="https://www.freepik.com/free-vector/coming-soon-design_951588.htm#fromView=keyword&page=1&position=9&uuid=0fc6d12c-b547-4b6d-9915-30901dab56c4&query=Website+building"
+            rel="noopener noreferrer"
+          >
+            rwdd_studios
+          </a>{" "}
+          on{" "}
+          <a
+            target="_blank"
+            href="https://www.freepik.com/"
+            rel="noopener noreferrer"
+          >
+            Freepik
+          </a>
+        </li>
+        <li>
+          Sign in background photo by{" "}
+          <a
+            target="_blank"
+            href="https://unsplash.com/@fakurian?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText"
+            rel="noopener noreferrer"
+          >
+            Milad Fakurian
+          </a>{" "}
+          on{" "}
+          <a
+            target="_blank"
+            href="https://unsplash.com/photos/a-close-up-of-a-fish-seA-FPPXL-M?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText"
+            rel="noopener noreferrer"
+          >
+            Unsplash
+          </a>
+        </li>
+        <li>
+          Triple T from{" "}
+          <a
+            target="_blank"
+            href="https://italianbrainrot.miraheze.org/wiki/@noxaasht"
+            rel="noopener noreferrer"
+          >
+            @noxaasht
+          </a>{" "}
+          on{" "}
+          <a
+            target="_blank"
+            href="https://tiktok.com/"
+            rel="noopener noreferrer"
+          >
+            TikTok
+          </a>
+        </li>
+        <li>
+          <a href="https://commons.wikimedia.org/wiki/File:Image-not-found.png">
+            Dang1702
+          </a>
+          ,{" "}
+          <a href="https://creativecommons.org/licenses/by-sa/4.0">
+            CC BY-SA 4.0
+          </a>
+          , via{" "}
+          <a
+            target="_blank"
+            href="https://commons.wikimedia.org/wiki/Main_Page"
+            rel="noopener noreferrer"
+          >
+            Wikimedia Commons
+          </a>{" "}
+          (Default no image found)
+        </li>
+        <li>
+          Brick wall photo by{" "}
+          <a
+            target="_blank"
+            href="https://unsplash.com/@rocinante_11?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText"
+            rel="noopener noreferrer"
+          >
+            Mick Haupt
+          </a>{" "}
+          on{" "}
+          <a
+            target="_blank"
+            href="https://unsplash.com/"
+            rel="noopener noreferrer"
+          >
+            Unsplash
+          </a>
+        </li>
+      </ul>
+    </>
+  );
 };
 
 export { Attributions };
